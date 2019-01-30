91ee4a0678c4d7a344174a2fe0237b0937f5a8a6
malloysm <malloysm@miamioh.edu>
2019-01-30 17:05:56 -0500

    Add files via upload

---
+++ SOS_LED.ino
@@ -0,0 +1,33 @@
+int LED = 2;
+
+ void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  S(); O(); S();
+  delay(1000);  
+}
+
+void S()
+{
+  for (int i=0; i <= 2; i++){
+    digitalWrite(LED,HIGH);
+    delay(200);
+    digitalWrite(LED,LOW);
+    delay(200);
+  }
+  delay(400);
+}
+
+void O()
+{
+  for (int i=0; i <= 2; i++){
+    digitalWrite(LED,HIGH);
+    delay(600);
+    digitalWrite(LED,LOW);
+    delay(200);
+  }
+  delay(400);
+}
